3b7a1256e35754c75dad5f85be751431150b2761
Mehmet Eren Ertürk <erturkmehmeteren@gmail.com>
2025-05-23 15:50:11 +0300

    "silme kısmında değişiklikler yapıldı, if else yerine case kullanıldı"

--- main.py
+++ main.py
@@ -14,83 +14,67 @@ while True :
     x = input("\nyapmak istediğiniz işlemi seçiniz")
 
 
-    if x == "1" :
+    match x:
+        case "1":
 
-        with open("kitaplar.json", "w") as f:
-            json.dump(mainkitaplar, f)
-        ad = input("ad giriniz")
+            with open("kitaplar.json", "w") as f:
+                json.dump(mainkitaplar, f)
+            ad = input("ad giriniz")
 
-        isbn = random.randint(1000000,9999999)
-        isbn = str(isbn)
+            isbn = random.randint(1000000,9999999)
+            isbn = str(isbn)
 
-        while True:
-            tarih = input("Tarihi 'gg/aa/yyyy' formatında girin: ")
-            try:
-                tarih = datetime.strptime(tarih, "%d/%m/%Y")
-                break
-            except ValueError:
-                print("Lütfen gg/aa/yyyy biçiminde tekrar girin.")
-        tarihstr = tarih.strftime("%d/%m/%Y")
-
-        kitaplar = {"ad": ad ,"tarih": tarihstr,"isbn" :isbn}
-        mainkitaplar.append(kitaplar)
-        with open("kitaplar.json", "w") as f:
-            json.dump(mainkitaplar, f)
-        print("kitap kaydedildi")
-
-
-    if x == "2" :
-        print("silmek istediğiniz kitabın bilgilerini giriniz")
-        while True:
-            silinenad = input("kitabın adını giriniz")
-            if silinenad in mainkitaplar:
-                print("bulundu")
-                break
-            else:
-                print("Kitap bilgilerini doğru giriniz")
-
-        while True:
-            silinentarih = input("Tarihi 'gg/aa/yyyy' formatında girin: ")
-            try:
-                silinentarih = datetime.strptime(silinentarih, "%d/%m/%Y")
-                break
-            except ValueError:
-                print("Lütfen gg/aa/yyyy biçiminde tekrar girin.")
-        silinentarihstr = silinentarih.strftime("%d/%m/%Y")
-
-        silinenisbn = input("kitabın isbn no sunu giriniz")
-        while True:
-            silinisbn = input("kitabın adını giriniz")
-            if silinisbn in mainkitaplar:
-                print("bulundu")
-                break
-            else:
-                print("İsbn hatalı")
-
-        sil = {"ad" : silinenad , "tarih" : silinentarihstr , "isbn" :silinenisbn }
-        if sil in mainkitaplar:
-            mainkitaplar.remove(sil)
-            print("Kitap başarıyla silindi.")
-        else:
-            print("Bu bilgilerle eşleşen bir kitap bulunamadı.")
-        with open("kitaplar.json", "w") as f:
-            json.dump(mainkitaplar, f)
-
-
-    if x == "3":
-        while True:
-            arananad = input("kitabın adını giriniz")
-            for a in mainkitaplar:
-                if arananad == a["ad"]:
-                    print("kitap bulundu\n")
-                    print("kitap adı:",a["ad"],"\n","yazılma tarihi:",a["tarih"],"\n","kitap isbn no:",a["isbn"],"\n")
+            while True:
+                tarih = input("Tarihi 'gg/aa/yyyy' formatında girin: ")
+                try:
+                    tarih = datetime.strptime(tarih, "%d/%m/%Y")
                     break
-            else:
-                print("Kitap bilgilerini doğru giriniz")
-                continue
+                except ValueError:
+                    print("Lütfen gg/aa/yyyy biçiminde tekrar girin.")
+            tarihstr = tarih.strftime("%d/%m/%Y")
+
+            kitaplar = {"ad": ad ,"tarih": tarihstr,"isbn" :isbn}
+            mainkitaplar.append(kitaplar)
+            with open("kitaplar.json", "w") as f:
+                json.dump(mainkitaplar, f)
+            print("kitap kaydedildi")
+
+
+        case "2":
+            print("silmek istediğiniz kitabın bilgilerini giriniz")
+            while True:
+                silinisbn = input("kitabın isbn no'sunu giriniz")
+                for b in mainkitaplar:
+                    if silinisbn == b["isbn"]:
+                        print("bulundu")
+                        sil = {"ad": b["ad"], "tarih": b["tarih"], "isbn": b["isbn"]}
+                        if sil in mainkitaplar:
+                            mainkitaplar.remove(sil)
+                            print("Kitap başarıyla silindi.")
+                        else:
+                            print("Bu bilgilerle eşleşen bir kitap bulunamadı.")
+                        with open("kitaplar.json", "w") as f:
+                            json.dump(mainkitaplar, f)
+                        break
+                    else:
+                        print("İsbn hatalı")
+                break
+
+        case "3":
+            while True:
+                arananad = input("kitabın adını giriniz")
+                for a in mainkitaplar:
+                    if arananad == a["ad"]:
+                        print("kitap bulundu\n")
+                        print("kitap adı:",a["ad"],"\n","yazılma tarihi:",a["tarih"],"\n","kitap isbn no:",a["isbn"],"\n")
+                        break
+                else:
+                    print("Kitap bilgilerini doğru giriniz")
+                    continue
+                break
+        case "4":
             break
-    if x == "4":
-        break
+
 
 
 
